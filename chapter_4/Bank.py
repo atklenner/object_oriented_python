c1c268d8ef33d5bb1b555eb53f3fa0b88aff5098
Andrew Klenner <atklenner@gmail.com>
2022-08-07 10:26:45 -0700

    add exceptions to bank class

--- chapter_4/Bank.py
+++ chapter_4/Bank.py
@@ -1,9 +1,31 @@
 from Account import *
 
+
 class Bank():
     def __init__(self) -> None:
         self.accounts_dict = {}
         self.next_account_number = 0
+
+    def ask_for_valid_account_number(self):
+        account_number = input("What is your account number? ")
+        try:
+            account_number = int(account_number)
+        except ValueError:
+            raise AbortTransaction("The account number must be an integer")
+        if account_number not in self.accounts_dict:
+            raise AbortTransaction(
+                f"There is no account {str(account_number)}")
+        return account_number
+
+    def ask_for_valid_password(self, account):
+        password = input("What is your password? ")
+        account.check_password(password)
+
+    def get_account(self):
+        account_number = self.ask_for_valid_account_number()
+        account = self.accounts_dict[account_number]
+        self.ask_for_valid_password(account)
+        return account
 
     def create_account(self, name, starting_balance, password):
         new_account = Account(name, password, starting_balance)
@@ -15,44 +37,37 @@ class Bank():
     def open_account(self):
         print("*** Open Account ***")
         user_name = input("What is the name for the new user account? ")
-        starting_balance = input("What is the stating balance for this account? ")
-        starting_balance = int(starting_balance)
-        password = input("What is the password you want to user for this account? ")
-        user_account_number = self.create_account(user_name, starting_balance, password)
+        starting_balance = input(
+            "What is the stating balance for this account? ")
+        password = input(
+            "What is the password you want to user for this account? ")
+        user_account_number = self.create_account(
+            user_name, starting_balance, password)
         print("Your new account number is:", user_account_number)
         print()
 
     def close_account(self):
         print("*** Close Account ***")
-        account_number = input("What is your account number? ")
-        account_number = int(account_number)
-        password = input("What is your password? ")
-        account = self.accounts_dict[account_number]
-        balance = account.get_balance(password)
+        account = self.get_account()
+        balance = account.get_balance()
         if balance:
-            print("You had", balance, "in your account, which is being returned to you.")
-        del self.accounts_dict[account_number]
+            print("You had", balance,
+                  "in your account, which is being returned to you.")
+        self.accounts_dict.pop(account)
         print("Your account is now closed")
 
     def balance(self):
         print("*** Get Balance ***")
-        account_number = input("Please enter your account number: ")
-        account_number = int(account_number)
-        password = input("Please enter the password: ")
-        account = self.accounts_dict[account_number]
-        balance = account.get_balance(password)
+        account = self.get_account()
+        balance = account.get_balance()
         if balance is not None:
             print("Your balance is: ", balance)
 
     def deposit(self):
         print("*** Deposit ***")
-        account_number = input("Please enter your account number: ")
-        account_number = int(account_number)
+        account = self.get_account()
         amount = input("Please enter the amount to deposit: ")
-        amount = int(amount)
-        password = input("Please enter the password: ")
-        account = self.accounts_dict[account_number]
-        balance = account.deposit(password, amount)
+        balance = account.deposit(amount)
         if balance is not None:
             print("Your new balance is:", balance)
 
@@ -65,13 +80,9 @@ class Bank():
 
     def withdraw(self):
         print("*** Withdraw ***")
-        account_number = input("Please enter your account number: ")
-        account_number = int(account_number)
+        account = self.get_account()
         amount = input("Please enter the amount to withdraw: ")
-        amount = int(amount)
-        password = input("Please enter the password: ")
-        account = self.accounts_dict[account_number]
-        balance = account.withdraw(password, amount)
+        balance = account.withdraw(amount)
         if balance is not None:
             print("Your new balance is:", balance)
 
