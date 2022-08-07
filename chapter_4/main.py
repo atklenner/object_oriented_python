a69d77cafc81313a71cd76d29ef4b2e22ce84dac
Andrew Klenner <atklenner@gmail.com>
2022-08-07 10:46:55 -0700

    handle exceptions in main loop

--- chapter_4/main.py
+++ chapter_4/main.py
@@ -2,11 +2,6 @@ from Bank import *
 
 bank = Bank()
 
-joe_account_number = bank.create_account('Joe', 100, 'JoesPassword')
-print("Joe's account number is:", joe_account_number)
-mary_account_number = bank.create_account('Mary', 12345, 'MarysPassword')
-print("Mary's account number is:", mary_account_number)
-
 while True:
     print()
     print('To get an account balance, press b')
@@ -19,22 +14,24 @@ while True:
     print()
     action = input('What do you want to do? ')
     action = action.lower()
-    action = action[0] # grab the first letter
+    action = action[0]  # grab the first letter
     print()
-    if action == 'b':
-        bank.balance()
-    elif action == 'c':
-        bank.closeAccount()
-    elif action == 'd':
-        bank.deposit()
-    elif action == 'o':
-        bank.openAccount()
-    elif action == 's':
-        bank.show()
-    elif action == 'q':
-        break
-    elif action == 'w':
-        bank.withdraw()
-    else:
-        print('Sorry, that was not a valid action. Please try again.')
-    print('Done')
+    try:
+        if action == 'b':
+            bank.balance()
+        elif action == 'c':
+            bank.close_account()
+        elif action == 'd':
+            bank.deposit()
+        elif action == 'o':
+            bank.open_account()
+        elif action == 's':
+            bank.show()
+        elif action == 'q':
+            break
+        elif action == 'w':
+            bank.withdraw()
+    except AbortTransaction as error:
+        print(error)
+
+print('Done')
