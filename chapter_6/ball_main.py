012693c5a5714666f23d01a7a46dbd7eafaa335d
Andrew Klenner <atklenner@gmail.com>
2022-08-08 14:36:34 -0700

    use all three classes

--- chapter_6/ball_main.py
+++ chapter_6/ball_main.py
@@ -3,13 +3,16 @@ from pygame.locals import *
 import sys
 from pathlib import Path
 from Ball import *
+from SimpleButton import *
+from SimpleText import *
 
 # define constants
 BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 WINDOW_WIDTH = 640
 WINDOW_HEIGHT = 480
 FRAMES_PER_SECOND = 30
-BASE_PATH = Path(__file__).resolve().parent
+BASE_PATH = str(Path(__file__).resolve().parent)
 N_BALLS = 30
 
 # initialize the window
@@ -20,9 +23,15 @@ clock = pygame.time.Clock()
 # load assets
 
 # initialize variables
-balls = []
-for i in range(N_BALLS):
-    balls.append(Ball(window, WINDOW_WIDTH, WINDOW_HEIGHT))
+restart_button_up = BASE_PATH + "/images/restart_button_up.png"
+restart_button_down = BASE_PATH + "/images/restart_button_down.png"
+ball = Ball(window, WINDOW_WIDTH, WINDOW_HEIGHT)
+frame_count_label = SimpleText(
+    window, (60, 20), "Program has run this many loops: ", WHITE)
+frame_count_display = SimpleText(window, (500, 20), "", WHITE)
+restart_button = SimpleButton(
+    window, (280, 60), restart_button_up, restart_button_down)
+frame_counter = 0
 
 # main loop
 while True:
@@ -32,16 +41,22 @@ while True:
             pygame.quit()
             sys.exit()
 
+        if restart_button.handle_event(event):
+            frame_counter = 0
+
     # per frame actions
-    for ball in balls:
-        ball.update()
+    ball.update()
+    frame_counter += 1
+    frame_count_display.set_value(str(frame_counter))
 
     # clear the window
     window.fill(BLACK)
 
     # draw all window elements
-    for ball in balls:
-        ball.draw()
+    ball.draw()
+    frame_count_label.draw()
+    frame_count_display.draw()
+    restart_button.draw()
 
     # update the window
     pygame.display.update()
