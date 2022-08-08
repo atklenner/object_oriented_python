a2e0519afd978fb65c54f88fe0bb1ab154bd2d42
Andrew Klenner <atklenner@gmail.com>
2022-08-07 19:58:42 -0700

    get rect

--- chapter_5/demo.py
+++ chapter_5/demo.py
@@ -10,12 +10,6 @@ WINDOW_WIDTH = 640
 WINDOW_HEIGHT = 480
 FRAMES_PER_SECOND = 30
 BASE_PATH = Path(__file__).resolve().parent
-BALL_WIDTH_HEIGHT = 100
-MAX_WIDTH = WINDOW_WIDTH - BALL_WIDTH_HEIGHT
-MAX_HEIGHT = WINDOW_HEIGHT - BALL_WIDTH_HEIGHT
-TARGET_X = 400
-TARGET_Y = 320
-TARGET_WIDTH_HEIGHT = 120
 N_PIXELS_TO_MOVE = 3
 
 # initialize the window
@@ -25,22 +19,20 @@ clock = pygame.time.Clock()
 
 # load assets
 path_to_ball = str(BASE_PATH) + "/images/ball.png"
-# path_to_target = str(BASE_PATH) + "/images/target.jpg"
 ball_image = pygame.image.load(path_to_ball)
-# target_image = pygame.image.load(path_to_target)
 
 # another way to do it
 # ball_image = pygame.image.load(
 #     Path("object_oriented_python/chapter_5/images/ball.png"))
 
 # initialize variables
-ball_x = random.randrange(MAX_WIDTH)
-ball_y = random.randrange(MAX_HEIGHT)
+ball_rect = ball_image.get_rect()
+MAX_WIDTH = WINDOW_WIDTH - ball_rect.width
+MAX_HEIGHT = WINDOW_HEIGHT - ball_rect.height
+ball_rect.left = random.randrange(MAX_WIDTH)
+ball_rect.top = random.randrange(MAX_HEIGHT)
 x_speed = N_PIXELS_TO_MOVE
 y_speed = N_PIXELS_TO_MOVE
-# ball_rect = pygame.Rect(ball_x, ball_y, BALL_WIDTH_HEIGHT, BALL_WIDTH_HEIGHT)
-# target_rect = pygame.Rect(
-# TARGET_X, TARGET_Y, TARGET_WIDTH_HEIGHT, TARGET_WIDTH_HEIGHT)
 
 # main loop
 while True:
@@ -50,39 +42,21 @@ while True:
             pygame.quit()
             sys.exit()
 
-    # key_pressed_tuple = pygame.key.get_pressed()
-
-    # if key_pressed_tuple[pygame.K_LEFT]:
-    #     ball_x -= N_PIXELS_TO_MOVE
-    # if key_pressed_tuple[pygame.K_RIGHT]:
-    #     ball_x += N_PIXELS_TO_MOVE
-    # if key_pressed_tuple[pygame.K_UP]:
-    #     ball_y -= N_PIXELS_TO_MOVE
-    # if key_pressed_tuple[pygame.K_DOWN]:
-    #     ball_y += N_PIXELS_TO_MOVE
-
-    if (ball_x < 0) or ball_x >= MAX_WIDTH:
+    # per frame actions
+    if ball_rect.left < 0 or ball_rect.right >= WINDOW_WIDTH:
         x_speed = -x_speed
 
-    if ball_y < 0 or ball_y >= MAX_HEIGHT:
+    if ball_rect.top < 0 or ball_rect.bottom >= WINDOW_HEIGHT:
         y_speed = -y_speed
 
-    ball_x += x_speed
-    ball_y += y_speed
-
-    # per frame actions
-    # ball_rect = pygame.Rect(
-    # ball_x, ball_y, BALL_WIDTH_HEIGHT, BALL_WIDTH_HEIGHT)
-
-    # if ball_rect.colliderect(target_rect):
-    # print("Ball is touching target")
+    ball_rect.left += x_speed
+    ball_rect.top += y_speed
 
     # clear the window
     window.fill(BLACK)
 
     # draw all window elements
-    # window.blit(target_image, (TARGET_X, TARGET_Y))
-    window.blit(ball_image, (ball_x, ball_y))
+    window.blit(ball_image, ball_rect)
 
     # update the window
     pygame.display.update()
