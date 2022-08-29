0b5daecf8f66e8facc2a7e1a634fe28b53b64f1a
Andrew Klenner <atklenner@gmail.com>
2022-08-28 19:50:31 -0700

    add game template

---
+++ chapter_12/high_or_low.py
@@ -0,0 +1,41 @@
+import pygame
+from pygame.locals import *
+import sys
+from pathlib import Path
+
+# define constants
+BLACK = (0, 0, 0)
+WINDOW_WIDTH = 640
+WINDOW_HEIGHT = 480
+FRAMES_PER_SECOND = 30
+BASE_PATH = str(Path(__file__).resolve().parent)
+
+# initialize the window
+pygame.init()
+window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+clock = pygame.time.Clock()
+
+# load assets
+
+# initialize variables
+
+# main loop
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    # per frame actions
+
+    # clear the window
+    window.fill(BLACK)
+
+    # draw all window elements
+
+    # update the window
+    pygame.display.update()
+
+    # set frame rate
+    clock.tick(FRAMES_PER_SECOND)
